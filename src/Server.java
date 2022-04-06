9ea42129b3f5feb1f670aa80f6d30a804d0e1109
Elias Ersson <32256443+Eliasers@users.noreply.github.com>
2022-04-06 10:26:27 +0200

    Create class that serves a loadable website

---
+++ src/Server.java
@@ -0,0 +1,35 @@
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class Server {
+
+    public static final int PORT = 80;
+
+    public static void main(String[] args) throws Exception {
+        Server s = new Server();
+        s.server.start();
+
+    }
+
+    HttpServer server;
+
+    public Server() throws Exception {
+        this.server = HttpServer.create(new InetSocketAddress(PORT), 0);
+
+        server.createContext("/", (HttpExchange t) -> {
+            String response =
+                    "<!doctype html>"+
+                            "van morrison was here </br>"+
+                            "Oh and hello, world";
+            byte[] bytes = response.getBytes();
+            t.sendResponseHeaders(200, bytes.length);
+            OutputStream os = t.getResponseBody();
+            os.write(bytes);
+            os.close();
+        });
+    }
+}
+
