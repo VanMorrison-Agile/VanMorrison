a7ea4ba0de7bc0d5e394e47ea56b2c1034a47979
André Kejovaara <74005387+Kejovaara@users.noreply.github.com>
2022-04-20 09:56:14 +0200

    Added example CSV and CSVReader class

---
+++ src/CSVReader.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class CSVReader {
+
+    String fileName;
+    public CSVReader(String fileName){
+        this.fileName = fileName;
+        readItemsFromCSV(fileName);
+    }
+
+    private static List<Item> readItemsFromCSV(String fileName){
+        List<Item> items = new ArrayList<Item>();
+        Path pathToFile = Paths.get(fileName);
+
+        try(BufferedReader br = Files.newBufferedReader(pathToFile, StandardCharsets.US_ASCII)){
+            String line = br.readLine();
+
+            while(line != null){
+                String[] attributes = line.split(",");
+
+                Item item = createItem(attributes);
+
+                items.add(item);
+
+                line = br.readLine();
+            }
+        }catch(IOException ioe){
+            ioe.printStackTrace();
+        }
+
+        return items;
+    }
+}
