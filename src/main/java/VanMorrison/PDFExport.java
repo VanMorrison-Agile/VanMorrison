91c3d1975aebb49e6ac5cc7febf0c007e322c981
Jonsson01 <emijon01@gmail.com>
2022-05-05 14:49:34 +0200

    Added new page and some separation between them

--- src/main/java/VanMorrison/PDFExport.java
+++ src/main/java/VanMorrison/PDFExport.java
@@ -7,6 +7,7 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
 import java.awt.Color;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
@@ -18,6 +19,7 @@ import org.apache.pdfbox.pdmodel.font.PDType1Font;
 import be.quodlibet.boxable.BaseTable;
 import be.quodlibet.boxable.Cell;
 import be.quodlibet.boxable.Row;
+
 
 public class PDFExport {
 
@@ -35,19 +37,20 @@ public class PDFExport {
             };
             cell.setFontSize(15);
         }
-
     }
 
     public static byte [] getPdf(List<Item> items) throws IOException {
 
         try (PDDocument document = new PDDocument()) {
 
-            // Create a document and add a page to it (A4 for printing)
+            // Create a document and add all pages to it (A4 for printing)
             PDPage productPage = new PDPage(PDRectangle.A4);
+            PDPage metadataPage = new PDPage(PDRectangle.A4);
             document.addPage(productPage);
+            document.addPage(metadataPage);
 
             // For product page
-            try (PDPageContentStream cos = new PDPageContentStream(document, productPage)) {
+            try (PDPageContentStream cont = new PDPageContentStream(document, productPage)) {
 
                 float margin = 50;
                 // starting y position is whole page height subtracted by top and bottom margin
@@ -61,17 +64,17 @@ public class PDFExport {
                 // y position is your coordinate of top left corner of the table
                 float yPosition = yStartNewPage;
 
-                BaseTable table = new BaseTable(yPosition, yStartNewPage,
+                BaseTable productTable = new BaseTable(yPosition, yStartNewPage,
                     bottomMargin, tableWidth, margin, document, productPage, true, drawContent);
 
                 // Row for the headers
                 List<String> headers = new ArrayList<String>();
                 headers.addAll(Arrays.asList("Vara", "Artikelnummer", "Antal"));
 
-                addRow(true, table, headers);
+                addRow(true, productTable, headers);
 
                 // Add all products/items  from provided list
-                items.forEach((item) -> addRow(false, table, item.getValues()));
+                items.forEach((item) -> addRow(false, productTable, item.getValues()));
 
                 Integer totalPrice = 0;
                 try {
@@ -83,13 +86,13 @@ public class PDFExport {
                 }
 
                 // Add last row for the total sum of prices
-                Row<PDPage> totalPriceRow = table.createRow(20);
+                Row<PDPage> totalPriceRow = productTable.createRow(20);
                 Cell<PDPage> cell = totalPriceRow.createCell(100/1.5f, "Total summa exkl. moms:");
                 cell.setFontSize(15);
                 cell = totalPriceRow.createCell(100f/3, totalPrice.toString());
                 cell.setFontSize(15);
 
-                table.draw();
+                productTable.draw();
             }
 
             // Saves pdf to an outputstream, which fills a byte array. Then returns byte array.
