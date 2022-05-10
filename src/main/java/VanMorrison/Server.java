f9f7f2313c7f1b7a232d08ddcd09d2e202606af5
Elias Ersson <eliasersson@hotmail.com>
2022-05-10 16:41:22 +0200

    More css, reformat and moved script tags from html to java

--- src/main/java/VanMorrison/Server.java
+++ src/main/java/VanMorrison/Server.java
@@ -132,7 +132,8 @@ public class Server {
             csv = new CSVReader("provider/" + provider + ".csv");
 
             
-            String extraScript = generateCartScript() + 
+            String extraScript = "<script>" + 
+            generateCartScript() + 
             """
                 var form = document.getElementById("sendOrderForm");
 
@@ -140,7 +141,8 @@ public class Server {
                 providerInput.setAttribute('name', 'provider');
                 providerInput.setAttribute('type', 'hidden');
                 providerInput.setAttribute('value', '""" + provider + "');" +
-                "form.appendChild(providerInput);";
+                "form.appendChild(providerInput);" +
+                "</script>";
 
 
             HtmlParser p = new HtmlParser("src/productView.html");
@@ -399,7 +401,7 @@ public class Server {
      * @return a complete html div as a string
      */
     public String generateCartDisplay() {
-        String cartDisplay = "<div>";
+        String cartDisplay = "<div id ='cart'>";
 
         for (Item item:
                 csv.items) {
