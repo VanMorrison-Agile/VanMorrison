a848bdbc90a439c6dfb54124f23dceba37916654
André Kejovaara <74005387+Kejovaara@users.noreply.github.com>
2022-05-19 15:24:11 +0200

    Added comments and removed redundant method addStyles()

--- src/main/java/VanMorrison/CSVReader.java
+++ src/main/java/VanMorrison/CSVReader.java
@@ -12,41 +12,48 @@ import java.util.List;
 
 public class CSVReader {
 
-    /*String fileName;
-    public CSVReader(String fileName){
-        this.fileName = fileName;
-        readItemsFromCSV(fileName);
-    }*/
     List<Item> items;
 
+    /**
+     * Reads a CSV file with a given path as a string
+     * @param fileName the path to the CSV file
+     */
     public CSVReader(String fileName){
         items = readItemsFromCSV(fileName);
     }
 
+    /**
+     * @param items list of items.
+     */
     public CSVReader(List<Item> items){
-
         this.items = items;
         //String[] meta = new String[]{"artNr","Namn","Pris"};
         this.items.add(0, createItem(new String[]{"artNr","Namn","Pris"}));
     }
 
+    /**
+     * @return the complete list including the header information
+     */
     public List<Item> getItemList(){
         return items;
     }
 
+    /**
+     * @return the itmes of a list, except the header information
+     */
     public List<Item> getItems(){
         return items.subList(1,items.size());
     }
 
+    /* Reads a CSV file and returns it as a list with the items with ArtNr,Name,Price */
     private static List<Item> readItemsFromCSV(String fileName){
         List<Item> items = new ArrayList<Item>();
         Path pathToFile = Paths.get(fileName);
         
         try(BufferedReader br = new BufferedReader(new FileReader(fileName));){
             String line = br.readLine();
-            System.out.println(line);
             while(line != null){
-                String[] attributes = line.split("[,;]");
+                String[] attributes = line.split("[,;]"); // Split to a new item at , or ;
 
                 Item item = createItem(attributes);
 
@@ -61,6 +68,7 @@ public class CSVReader {
         return items;
     }
 
+    /* Create the item metadata from the items read in the CSV file */
     private static Item createItem(String[] metadata){
         String artNr = metadata[0];
         String name = metadata[1];
@@ -69,11 +77,14 @@ public class CSVReader {
         return new Item(artNr,name,price);
     }
 
+    /* Returns the list as a correct string */
     public String printToString(){
         String s = generateList();
         return s;
     }
 
+    /* Generates a string of HTML with a correct table structure */
+    /* Uses the methods generateTableRows() and generateTableHead() to input the items */
     private String generateList(){
         String s = "<br />" +
                     " <table>" +
@@ -85,6 +96,7 @@ public class CSVReader {
         return s;
     }
 
+    /* Generates the rows of the table given a list of items */
     private String generateTableRows(){
         String s = "";
         for(int i = 1; i < getItemList().size(); i++){
@@ -93,80 +105,10 @@ public class CSVReader {
         return s;
     }
 
+    /* Generates the header of the table for a fixed metadata structure */
     private String generateTableHead(){
         String s = "<th>";
         s += getItemList().get(0).getArtNr() + "</th><th>" + getItemList().get(0).getName() + "</th><th>" + getItemList().get(0).getPrice() + "</th><th></th>";
-        return s;
-    }
-
-    public String getStyle(){
-        String s = "<style>" +
-                "html,\n" +
-                "body {\n" +
-                "\theight: 100%;\n" +
-                "}\n" +
-                "\n" +
-                "body {\n" +
-                "\tmargin: 0;\n" +
-                "\tbackground: linear-gradient(45deg, #49a09d, #5f2c82);\n" +
-                "\tfont-family: sans-serif;\n" +
-                "\tfont-weight: 100;\n" +
-                "}\n" +
-                "\n" +
-                ".container {\n" +
-                "\tposition: absolute;\n" +
-                "\ttop: 50%;\n" +
-                "\tleft: 50%;\n" +
-                "\ttransform: translate(-50%, -50%);\n" +
-                "}\n" +
-                "\n" +
-                "table {\n" +
-                "\twidth: 800px;\n" +
-                "\tborder-collapse: collapse;\n" +
-                "\toverflow: hidden;\n" +
-                "\tbox-shadow: 0 0 20px rgba(0,0,0,0.1);\n" +
-                "}\n" +
-                "\n" +
-                "th,\n" +
-                "td {\n" +
-                "\tpadding: 15px;\n" +
-                "\tbackground-color: rgba(255,255,255,0.2);\n" +
-                "\tcolor: #fff;\n" +
-                "}\n" +
-                "\n" +
-                "th {\n" +
-                "\ttext-align: left;\n" +
-                "}\n" +
-                "\n" +
-                "thead {\n" +
-                "\tth {\n" +
-                "\t\tbackground-color: #55608f;\n" +
-                "\t}\n" +
-                "}\n" +
-                "\n" +
-                "tbody {\n" +
-                "\ttr {\n" +
-                "\t\t&:hover {\n" +
-                "\t\t\tbackground-color: rgba(255,255,255,0.3);\n" +
-                "\t\t}\n" +
-                "\t}\n" +
-                "\ttd {\n" +
-                "\t\tposition: relative;\n" +
-                "\t\t&:hover {\n" +
-                "\t\t\t&:before {\n" +
-                "\t\t\t\tcontent: \"\";\n" +
-                "\t\t\t\tposition: absolute;\n" +
-                "\t\t\t\tleft: 0;\n" +
-                "\t\t\t\tright: 0;\n" +
-                "\t\t\t\ttop: -9999px;\n" +
-                "\t\t\t\tbottom: -9999px;\n" +
-                "\t\t\t\tbackground-color: rgba(255,255,255,0.2);\n" +
-                "\t\t\t\tz-index: -1;\n" +
-                "\t\t\t}\n" +
-                "\t\t}\n" +
-                "\t}\n" +
-                "}" +
-                "</style>";
         return s;
     }
 }
